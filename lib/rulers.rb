edb9106e677a40f9fdc0cac6662ae47f3e3fb715
Hoang Cong Minh <hoang.cong.minh@framgia.com>
2014-10-08 18:45:43 +0700

    nghi ngay thu 1 2014-10-08

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,9 +1,11 @@
 require "rulers/version"
 require "rulers/array"
 
-class Rulers::Application
-  def call env
-    `echo debug > debug.txt`;
-    [200, {"Content-Type" => "text/html"}, ["Hello from Ruby on Rulers"]]
+module Rulers
+  class Application
+    def call env
+      `echo debug > debug.txt`;
+      [200, {"Content-Type" => "text/html"}, ["Hello from Ruby on Rulers"]]
+    end
   end
 end
